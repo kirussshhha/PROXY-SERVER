a91c07691d8fefada11918a1a43647dddbe8b5f3
kirussshhha <kirill2006may@gmail.com>
2024-11-15 15:01:26 +0300

    PRO-6: Add filter on json data

--- index.js
+++ index.js
@@ -14,7 +14,28 @@ app.get("/meteors", (req, res) => {
       res.send(
         `Количество замеченых астероидов с понедельника по пятницу: ${data.element_count}`
       );
-      console.log(data);
+      const nearEarthObjects = data.near_earth_objects;
+      const filteredMeteors = [];
+
+      for (const date in nearEarthObjects) {
+        nearEarthObjects[date].forEach((meteor) => {
+          const meteorInfo = {
+            id: meteor.id,
+            name: meteor.name,
+            diameter: meteor.estimated_diameter.meters,
+            is_potentially_hazardous_asteroid:
+              meteor.is_potentially_hazardous_asteroid,
+            close_approach_date_full:
+              meteor.close_approach_data[0].close_approach_date_full,
+            relative_velocity:
+              meteor.close_approach_data[0].relative_velocity
+                .kilometers_per_second,
+          };
+          filteredMeteors.push(meteorInfo);
+        });
+      }
+
+      console.log(filteredMeteors);
     })
     .catch((error) => {
       console.error("Ошибка при запросе к NASA API:", error.message);
