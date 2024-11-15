201054e6581f317c9bfaecdf8386b016015045b8
kirussshhha <kirill2006may@gmail.com>
2024-11-15 13:20:57 +0300

    PRO-4: Add end-point /meteors

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.get("/meteors", (req, res) => {
+  const startDate = "2024-11-11";
+  const endDate = "2024-11-15";
+  const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${startDate}&end_date=${endDate}&api_key=${process.env.NASA_API_KEY}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      res.send(
+        `Количество замеченых астероидов с понедельника по пятницу: ${data.element_count}`
+      );
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("Ошибка при запросе к NASA API:", error.message);
+      res.status(500).send("Ошибка при запросе к NASA API");
+    });
 });
 
 app.listen(port, () => {
