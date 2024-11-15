2417bb084ca232968667961ec38702d620d3845a
kirussshhha <kirill2006may@gmail.com>
2024-11-15 11:40:08 +0300

    PRO-2: Connect express

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`server listen on port ${port}`);
+});
