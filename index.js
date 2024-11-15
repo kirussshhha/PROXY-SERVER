2ec8c2ee1f6a60a59885d713ff8f6ee45d75a3b4
kirussshhha <kirill2006may@gmail.com>
2024-11-15 12:00:12 +0300

    PRO-3: Add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.listen(port, () => {
   console.log(`server listen on port ${port}`);
